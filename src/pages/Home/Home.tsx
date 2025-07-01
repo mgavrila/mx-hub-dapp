411b9d0a0f3561c0f3729d8d27131f84761ab8b8
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-07-01 11:13:28 +0300

    Remove components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import { PageWrapper } from 'wrappers';
-import { Transaction } from './Transaction';
 
 export const Home = () => {
   return (
@@ -9,29 +8,8 @@ export const Home = () => {
         <div className='flex items-start sm:items-center h-full sm:w-1/2 sm:bg-center'>
           <div className='flex flex-col gap-2 max-w-[70sch] text-center sm:text-left text-xl font-medium md:text-2xl lg:text-3xl'>
             <div>
-              <h1>Template dApp</h1>
-              <p className='text-gray-400'>
-                The{' '}
-                <a
-                  href='https://www.npmjs.com/package/@multiversx/sdk-dapp'
-                  target='_blank'
-                  className='text-gray-400 underline decoration-dotted hover:decoration-solid'
-                >
-                  sdk-dapp
-                </a>{' '}
-                starter project for any dApp <br className='hidden xl:block' />
-                built on the{' '}
-                <a
-                  href='https://multiversx.com/'
-                  target='_blank'
-                  className='text-gray-400 underline decoration-dotted hover:decoration-solid'
-                >
-                  MultiversX
-                </a>{' '}
-                blockchain.
-              </p>
+              <h1>Hub dApp</h1>
             </div>
-            <Transaction />
           </div>
         </div>
         <div className='h-4/6 bg-mvx-white bg-contain bg-no-repeat w-1/2 bg-center' />
